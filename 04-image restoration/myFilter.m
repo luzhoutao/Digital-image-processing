a0827c5b0b50719d170529b2a8b2bba4ba8c44d4
luzhoutao <lztPublicMail@163.com>
2017-01-22 16:27:20 +0800

    delete lab4

--- 04-image restoration/myFilter.m
+++
@@ -1,6 +0,0 @@
-function [ output_freq ] = myFilter( freq, filter )
-% filter in the frequency domain
-    output_freq = freq.*filter;
-    imshow(im2uint8(mat2gray(log(abs(output_freq)+1))));
-end
-
--- 04-image restoration/transform.m
+++
@@ -1,6 +0,0 @@
-function [ F ] = transform( image )
-% transform the image to frequency domain
-    F = fft2(image);
-    %imshow(im2uint8(mat2gray(log(abs(F)+1))));
-end
-
